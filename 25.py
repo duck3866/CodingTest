45692ffdf6671cc9b0975031819bc5e1bcbb37a9
duck3866 <130462482+duck3866@users.noreply.github.com>
2023-05-25 21:58:43 +0900

    Add files via upload

--- 25.py
+++ 25.py
@@ -1,10 +1,7 @@
-car=170
-a,b,c=map(int,input().split())
-if a<=car :
-    print("CRASH %d"%a)
-elif b<=car :
-    print("CRASH %d"%b)
-elif c<=car:
-    print("CRASH %d"%c)
-else:
-    print("PASS")
+import random
+print("%d번" %(random.randint(1, 5)))
+a = random.sample(range(1,6), 5)
+for i in a:
+    print(i,'번')
+
+
