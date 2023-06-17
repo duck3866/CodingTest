21dfd75e599f79f10714dfe16993b14f85793f23
duck3866 <130462482+duck3866@users.noreply.github.com>
2023-06-17 21:30:18 +0900

    Add files via upload

---
+++ 6-17(ㅗ).py
@@ -0,0 +1,80 @@
+word=input()
+a=0
+b=0
+c=0
+d=0
+e=0
+f=0
+g=0
+h=0
+ii=0
+j=0
+k=0
+l=0
+n=0
+m=0
+o=0
+p=0
+q=0
+r=0
+t=0
+u=0
+v=0
+w=0
+x=0
+y=0
+z=0
+for i in word:
+	if i=='a':
+		a+=1
+	elif i=='b':
+		b+=1
+	elif i=='c':
+		c+=1
+	elif i=='d':
+		d+=1
+	elif i=='e':
+		e+=1
+	elif i=='f':
+		f+=1
+	elif i=='g':
+		g+=1
+	elif i=='h':
+		h+=1
+	elif i=='i':
+		ii+=1
+	elif i=='k':
+		k+=1
+	elif i=='l':
+		l+=1
+	elif i=='n':
+		n+=1
+	elif i=='m':
+		m+=1
+	elif i=='o':
+		o+=1
+	elif i=='p':
+		p+=1
+	elif i=='q':
+		q+=1
+	elif i=='r':
+		r+=1
+	elif i=='t':
+		t+=1
+	elif i=='u':
+		u+=1
+	elif i=='v':
+		v+=1
+	elif i=='w':
+		w+=1
+	elif i=='x':
+		x+=1
+	elif i=='y':
+		y+=1
+	elif i=='z':
+		z+=1
+print(a,b,c,d,e,f,g,h,i,j,k,l,n,m,o,p,q,r,t,u,v,w,x,y,z)
+
+
+		
+	
