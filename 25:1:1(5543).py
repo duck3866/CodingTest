a10c426adf5c1d869cd73b893ba0843ac0ec2927
duck3866 <130462482+duck3866@users.noreply.github.com>
2025-01-01 23:54:42 +0900

    25:1:1(5543,상근날드,브4)
    매우 이지

---
+++ 25:1:1(5543).py
@@ -0,0 +1,8 @@
+bug = []
+bug.append(int(input()))
+bug.append(int(input()))
+bug.append(int(input()))
+drink = []
+drink.append(int(input()))
+drink.append(int(input()))
+print(min(bug)+min(drink) -50)
